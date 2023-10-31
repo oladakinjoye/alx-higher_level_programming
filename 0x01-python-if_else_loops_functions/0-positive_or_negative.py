8554b1c92d7617d0b2435c2e98a11effc8fecd3e
oladakinjoye <oladakinjoye@gmail.com>
2023-10-31 13:05:05 -0700

    0-positive_or_negative.py file 1

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -5,5 +5,5 @@ if number > 0:
     print("{0:d} is positive".format(number))
 elif number == 0:
     print("{0:d} is  zero".format(number))
-else: 
+else:
     print("{0:d} is negative".format(number))
