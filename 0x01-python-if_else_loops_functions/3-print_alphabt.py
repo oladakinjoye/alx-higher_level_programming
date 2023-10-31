33d6103db5136a81f9f97e26d4d54a4c0ce146ce
oladakinjoye <oladakinjoye@gmail.com>
2023-10-31 14:02:11 -0700

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z')+1):
-    if chr(i) not in ['q', 'e']:
-        print(chr(i), end='')
+for char in range(97, 123):
+    if (char != 101 and char != 113):
+        print("{:c}".format(char), end='')
