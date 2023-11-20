d0ac4014a4be2076559476b56432e6cb6ad2755d
oladakinjoye <oladakinjoye@gmail.com>
2023-11-20 14:17:49 -0800

    ask 102 magic

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    result = 0
+    for j in range(1, 3):
+        try:
+            if j > a:
+                raise Exception('Too far')
+            result += a ** b / j
+        except Exception:
+            result = b + a
+            break
+    return result
