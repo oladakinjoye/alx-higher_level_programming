9ae4d1bfb51f1d114f09f6a47c0899efd860ecd1
oladakinjoye <oladakinjoye@gmail.com>
2023-11-20 13:36:35 -0800

    upadte task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    
-   
+    """Print x elememts of a list.
+    Args:
+        my_list (list): list to print elements.
+        x (int): number of elements of my_list to print.
+
+    Returns:
+        number of elements printed.
+ """ 
     ret = 0
     for j in range(x):
         try:
