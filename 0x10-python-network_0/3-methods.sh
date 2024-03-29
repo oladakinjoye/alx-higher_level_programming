0aec6001f2cef443b2ca577856cb27cf9ed932c0
oladakinjoye <oladakinjoye@gmail.com>
2024-03-29 13:46:24 -0700

    3 methods1

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Display all HTTP methods the server will accept using curl.
+# display all HTTP methods the server will accept using curl.
 curl -sI "$1" | grep "ALLOW" | cut -d " " -f 2-
